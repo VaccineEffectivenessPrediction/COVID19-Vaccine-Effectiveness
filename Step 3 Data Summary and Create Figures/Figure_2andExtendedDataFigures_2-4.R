e4d1ff5647b1a47200c835f28bf295ce8e63afe0
VaccineEffectivenessAnalytics <105188633+VaccineEffectivenessAnalytics@users.noreply.github.com>
2022-05-10 00:17:07 +0800

    Update Figure_2andExtendedDataFigures_2-4.R

--- Step 3 Data Summary and Create Figures/Figure_2andExtendedDataFigures_2-4.R
+++ Step 3 Data Summary and Create Figures/Figure_2andExtendedDataFigures_2-4.R
@@ -11,7 +11,7 @@ lm.sp <- function(df1,x1,y1,group1,type1,title) {
                 se=FALSE,linetype="dashed") + 
     geom_point(data=df1, aes(x = x1, y = y1,color=group1,shape=type1),size=3.6) 
   
-  splot.edited <- splot +
+  splot_edited <- splot +
     scale_y_continuous(name="COVID-19 VE (%)",breaks=seq(0, 100, 25)) + scale_x_continuous(name = "Genetic distance") + 
     scale_colour_manual(values = cols_lmsp) + theme_classic() + 
     scale_fill_manual(values = cols_lmsp) +
@@ -20,7 +20,7 @@ lm.sp <- function(df1,x1,y1,group1,type1,title) {
     theme(legend.position = "none") +
     expand_limits(y = 0)
   print(splot.edited)
-  ggsave(filename = paste(title,".png",sep=""), plot = splot.edited,width = 4.28, height =3)
+  ggsave(filename = paste(title,".png",sep=""), plot = splot_edited,width = 4.28, height =3)
 }
 
 lm.sp(df,df$RBD,df$VE,df$platforms,df$type,"(a) RBD")
@@ -39,21 +39,21 @@ sp <- function(df1,x1,y1,type1,title) {
     theme(legend.position = "none") +
     expand_limits(y = c(0,100),x=c(0,3))
   print(splot.edited)
-  ggsave(filename = paste(title,".png",sep=""), plot = splot.edited,width = 4.28, height =3)
+  ggsave(filename = paste(title,".png",sep=""), plot = splot_edited,width = 4.28, height =3)
 }
 
 sp.nse <- function(df1,x1,y1,type1,title) {
   splot <- ggplot(data=df1, aes(x = x1, y = y1,shape=type1)) +
     geom_point(color="#00C091",size=3.6)
   
-  splot.edited <- splot + 
+  splot_edited <- splot + 
     scale_y_continuous(name="COVID-19 VE (%)",breaks=seq(0, 100, 25)) + scale_x_continuous(name = "Genetic distance",breaks=seq(0, 3, 1)) +
     theme_classic() + labs(title = title) +
     theme(plot.title = element_text(size=12, face="bold")) +
     theme(legend.position = "none") +
     expand_limits(y = c(0,100),x=c(0,3))
-  print(splot.edited)
-  ggsave(filename = paste(title,".png",sep=""), plot = splot.edited,width = 4.28, height =3)
+  print(splot_edited)
+  ggsave(filename = paste(title,".png",sep=""), plot = splot_edited,width = 4.28, height =3)
 }
 
 df1 <- subset(df,developer=="BNT")
@@ -88,14 +88,14 @@ cols_psp <- c("#00BFC4","#F8766D","#FFB90F","#C77CFF")
 p.sp <- function(df1,x1,y1,group1,type1,title) {
   splot <- ggplot() + geom_point(data=df1, aes(x = x1, y = y1,colour=group1,shape=type1),size=3.6) 
   
-  splot.edited <- splot + 
+  splot_edited <- splot + 
     scale_y_continuous(name="COVID-19 VE (%)") + scale_x_continuous(name = "Genetic distance") + 
     scale_colour_manual(values = cols_psp) + 
     theme_classic() + labs(title = title) + 
     theme(plot.title = element_text(size=12, face="bold")) + 
     theme(legend.position = "none") + expand_limits(y = 0)
   print(splot.edited)
-  ggsave(filename = paste(title,".png",sep=""), plot = splot.edited,width = 4.28, height =3)
+  ggsave(filename = paste(title,".png",sep=""), plot = splot_edited,width = 4.28, height =3)
 }
 
 p.sp(df,df$ORF1a,df$VE,df$platforms,df$type,"(a) ORF1a")
